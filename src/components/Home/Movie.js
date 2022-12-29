b7d7fc9cbb205feb1a6006336837c4cf076c93cb
ikhsanse <ikhsansolihin49@gmail.com>
2022-12-29 10:10:21 +0700

    fixing

--- src/components/Home/Movie.js
+++ src/components/Home/Movie.js
@@ -5,8 +5,17 @@ const baseImg = process.env.REACT_APP_IMAGE_500_END_POINT;
 const Movie = ({ movie, index }) => {
   const navigate = useNavigate();
   const detailNavigate = () => {
-    navigate(`/movie/detail/${index}-${movie?.id}`)
-  }
+    navigate(`/movie/detail/${index}-${movie?.id}`);
+  };
+
+  const truncateString = (str, num) => {
+    if (str?.length > num) {
+      return str.slice(0, num) + "...";
+    } else {
+      return str;
+    }
+  };
+
   return (
     <>
       <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2">
@@ -16,8 +25,11 @@ const Movie = ({ movie, index }) => {
           alt={movie?.title}
         />
         <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
-          <p onClick={detailNavigate} className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
-            {movie?.title}
+          <p
+            onClick={detailNavigate}
+            className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center w-full h-full text-center"
+          >
+            {truncateString(movie?.title, 30)}
           </p>
         </div>
       </div>
